66eb269840b999568a89692af3b68d9afd41dc50
Aleksandar Matevski <matevskial@gmail.com>
2025-02-16 01:33:17 +0100

    Implement basic eval of expression
    Note: the decimal number is currently converted to long

--- main.c
+++ main.c
@@ -22,15 +22,60 @@ static char *chapter_6_bonus_mark_language =
     ";
 
 static char *my_own_lisp_language =
-    "                                                                         \
-        number          : /-?[0-9]+/ ;                                        \
-        operator        : '+' | '-' | '*' | '/' | ;                           \
-        expr            : <number> | '(' <operator> <expr>+ ')' ;             \
-        my_own_lisp     : /^/ <operator> <expr>+ /$/ ;                        \
+    "                                                                                            \
+        number          : /-?[0-9]+/ ;                                                           \
+        decimal_number  : /-?[0-9]*\\.[0-9]+/ ;                                                   \
+        operator        : '+' | '-' | '*' | '/' | '%' ;                                          \
+        expr            : <decimal_number> | <number> | '(' <operator> <expr>+ ')' ;             \
+        my_own_lisp     : /^/ <operator> <expr>+ /$/ ;                                           \
     ";
+
+bool has_tag(mpc_ast_t * ast, char * tag) {
+    return strstr(ast->tag, tag) != NULL;
+}
+
+long execute_operation(mpc_ast_t * operator, long first_operand, long second_operand) {
+    if (strcmp(operator->contents, "+") == 0) {
+        return first_operand + second_operand;
+    }
+    if (strcmp(operator->contents, "-") == 0) {
+        return first_operand - second_operand;
+    }
+    if (strcmp(operator->contents, "*") == 0) {
+        return first_operand * second_operand;
+    }
+    if (strcmp(operator->contents, "/") == 0) {
+        return first_operand / second_operand;
+    }
+    return 0;
+}
+
+/* Assumes ast is not NULL */
+long eval(mpc_ast_t *ast) {
+    if (has_tag(ast, "number")) {
+        return atoi(ast->contents);
+    }
+    if (has_tag(ast, "decimal_number")) {
+        return (long) atof(ast->contents);
+    }
+
+    mpc_ast_t *operator = ast->children[1];
+    long first_expr_value = eval(ast->children[2]);
+    long result = first_expr_value;
+
+    int i = 3;
+    while (has_tag(ast->children[i], "expr")) {
+        long current_expr_value = eval(ast->children[i]);
+        result = execute_operation(operator, result, current_expr_value);
+        i++;
+    }
+
+    return result;
+}
 
 int main(int argc, char** argv) {
     mpc_parser_t* number = mpc_new("number");
+    mpc_parser_t* decimal_number = mpc_new("decimal_number");
     mpc_parser_t* operator = mpc_new("operator");
     mpc_parser_t* expr = mpc_new("expr");
     mpc_parser_t* my_own_lisp = mpc_new("my_own_lisp");
@@ -38,7 +83,7 @@ int main(int argc, char** argv) {
     mpca_lang(
         MPCA_LANG_DEFAULT,
         my_own_lisp_language,
-       number, operator, expr, my_own_lisp
+       number, decimal_number, operator, expr, my_own_lisp
     );
 
     puts("my-own-lisp version 0.0.1");
@@ -54,15 +99,12 @@ int main(int argc, char** argv) {
 
         mpc_result_t my_own_lisp_parse_result;
         if (mpc_parse("<stdin>", input_buff, my_own_lisp, &my_own_lisp_parse_result)) {
-            mpc_ast_print(my_own_lisp_parse_result.output);
+            long result = eval(my_own_lisp_parse_result.output);
+            printf("%ld\n", result);
             mpc_ast_delete(my_own_lisp_parse_result.output);
         } else {
             mpc_err_print(my_own_lisp_parse_result.error);
             mpc_err_delete(my_own_lisp_parse_result.error);
-        }
-
-        if (size_read > 1) {
-            printf("echo back: %s\n", input_buff);
         }
     }
 
